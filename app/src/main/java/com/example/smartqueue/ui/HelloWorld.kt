f98f2427d04948797a302ae33272614601041c51
tejasM17 <tejasm9380@gmail.com>
2026-09-24 10:49:50 +0530

    fix build

---
+++ app/src/main/java/com/example/smartqueue/ui/HelloWorld.kt
@@ -0,0 +1,10 @@
+package com.example.smartqueue.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Hello(modifier: Modifier = Modifier) {
+    Text(text = "Hello, World!")
+}
